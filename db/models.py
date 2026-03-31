08c443ff236cec60d69ed1a297eec69f6cf141c3
Vladislav <ilichoff88@gmail.com>
2026-03-31 18:07:57 +0300

    Solution

--- db/models.py
+++ db/models.py
@@ -6,6 +6,10 @@ class CinemaHall(models.Model):
     rows = models.IntegerField()
     seats_in_row = models.IntegerField()
 
+    @property
+    def capacity(self) -> int:
+        return self.rows * self.seats_in_row
+
     def __str__(self) -> str:
         return self.name
 
